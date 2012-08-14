2cc8da15706df9d5c1629f6adc2ca97323e43a97
Sony <Sony@Sony-VAIO>
2012-08-14 16:43:04 +0530

    able to post and get data from JSON

--- src/com/example/json3/MainActivity.java
+++ src/com/example/json3/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.json3;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,6 +33,7 @@ public class MainActivity extends Activity {
 	TextView tv;
 	HttpClient client;
 	JSONObject json;
+	List<NameValuePair> params;
 final static String URL = "http://192.168.1.9/sumit/hello.php";
 	//final static String URL = "http://json.org/example.html";
 	@Override
@@ -37,33 +45,54 @@ final static String URL = "http://192.168.1.9/sumit/hello.php";
 		client = new DefaultHttpClient();
 		
 		Log.e("my","check");
-		new Read().execute("tag");
-//		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
-//				.detectAll().penaltyLog().build();
-//		StrictMode.setThreadPolicy(policy);
+		
 
+		
+		params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("tag", "Hola"));
+		params.add( new BasicNameValuePair("msg", "Hooah"));
+		
+		new Read().execute("user");
+	
 	}
-
-	public String myData() throws ClientProtocolException, IOException,
-			JSONException {
-
-		StringBuilder url = new StringBuilder(URL);
-		HttpGet get = new HttpGet(url.toString());
-		HttpResponse r = client.execute(get);
+	
+	public JSONObject sendJSONtoUrl() throws ClientProtocolException, IOException, JSONException{
+		
+		DefaultHttpClient httpClient = new DefaultHttpClient();
+		HttpPost httpPost = new HttpPost(URL);
+		try {
+			httpPost.setEntity( new UrlEncodedFormEntity(params));
+			
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		HttpResponse r = client.execute(httpPost);
 		int status = r.getStatusLine().getStatusCode();
 		if (status == 200) {
+			Log.e("my","status verified");
 			HttpEntity e = r.getEntity();
 			String data = EntityUtils.toString(e);
-//			JSONArray datastream = new JSONArray(data);
-//			JSONObject message = datastream.getJSONObject(0);
-//			return message;
-			return data;
-		} else {
-			Toast.makeText(MainActivity.this, "error encountered",
-					Toast.LENGTH_SHORT).show();
-			return null;
-		}
 
+			//This works fine--------------------
+						JSONArray datastream1 = new JSONArray(data);
+			JSONObject message = datastream1.getJSONObject(0);
+			
+			return message;
+		
+		}
+		else{
+		return null;
+		
+		}
+		
+		
+		
+		
+		///++++++++++++++
 	}
 
 	class Read extends AsyncTask<String, Integer, String> {
@@ -72,20 +101,28 @@ final static String URL = "http://192.168.1.9/sumit/hello.php";
 		protected String doInBackground(String... params) {
 			// TODO Auto-generated method stub
 			try {
-				//json = myData();
+				json = sendJSONtoUrl();
 				
-								//	return json.getString(params[0]);
-				String s = myData();
-				return s;
+									return json.getString(params[0]);
+//				String s = myData();
+//				return s;
 				
 				
+			} 
+//			catch (ClientProtocolException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+			catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			} catch (ClientProtocolException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
@@ -97,9 +134,11 @@ final static String URL = "http://192.168.1.9/sumit/hello.php";
 		protected void onPostExecute(String result) {
 			// TODO Auto-generated method stub
 			tv.setText(result);
-
 		}
 
 	}
+	
+	
+
 
 }
